5aeec3e66575f218498063d67a5e0882a907f4e4
Pedro Capela <pepe.capela@exemplo.com>
2026-03-25 09:37:24 -0300

    Sincronização automática

---
+++ Bioinformatics Stronghold/rosalind_LCSM.py
@@ -0,0 +1,18 @@
+from Bio import SeqIO
+
+def find_lcs(fasta_file):
+    records = list(SeqIO.parse(fasta_file, "fasta"))
+    seqs = [str(r.seq) for r in records]
+
+    seqs.sort(key=len)
+    shortest_seq = seqs[0]
+    n = len(shortest_seq)
+
+    for length in range(n, 0, -1):
+        for start in range(n - length + 1):
+            susbtring = shortest_seq[start:start + length]
+            if all(susbtring in s for s in seqs[1:]):
+                return susbtring
+    return ""
+
+print(find_lcs("dados_LCSM.txt"))
